d606fc5158f9a18f164953b92ec3d43f7714cd92
Alexa Sih <alexa17cassidy@gmail.com>
2019-07-01 15:17:25 -0700

    realtime chat between users

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -20,10 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //TODO: Initialise and Configure your Firebase here:
         FirebaseApp.configure()
         
-        let myDataBase = Database.database().reference()
-        
-        myDataBase.setValue("We've got data!")
-        
         return true
     }
 
